912e35e49e157e1c9526732389e0dc0d68061177
“Ray” <ray00211@gmail.com>
2022-10-02 20:14:20 +0800

    完成灰色照片顯示，QImage.Format、通道數量bytesPerline要設定

--- HW/HW02/controller.py
+++ HW/HW02/controller.py
@@ -24,8 +24,7 @@ class MainWindow_controller(QtWidgets.QMainWindow):
     def setup_control(self):
         # TODO
 
-        self.ui.button_file.clicked.connect(self.open_and_return_filename)
-        # self.ui.button_file.clicked.connect(self.display_img)
+        self.ui.button_file.clicked.connect(self.Q2_1_open_file)
         self.ui.button_2_2_grayscale.clicked.connect(self.Q2_2_grayscale)
         self.ui.button_2_3_histogram.clicked.connect(self.Q2_3_histogram)
         self.ui.button_2_4_threshold.clicked.connect(self.Q2_4_threshold)
@@ -39,74 +38,112 @@ class MainWindow_controller(QtWidgets.QMainWindow):
 
 
 
-    def display_img(self, img_name, objectName):
-        if type(img_name) == str: # 路徑的話就做imread,否則直接使用
-            self.img = cv2.imread(img_name)
-        elif type(img_name) == np.ndarray:
-             self.img = img_name
+    # def display_img(self, img_name, objectName):
+        # if type(img_name) == str: # 路徑的話就做imread,否則直接使用
+        #     self.img = cv2.imread(img_name)
+        # elif type(img_name) == np.ndarray:
+        #      self.img = img_name
 
-        self.img = cv2.resize(self.img, (200,150))
-        height, width = self.img.shape[0], self.img.shape[1]
-        bytesPerline = 3 * width
-        self.qimg = QImage(self.img, width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
+        # self.img = cv2.resize(self.img, (200,150))
+        # height, width = self.img.shape[0], self.img.shape[1]
+        # bytesPerline = 3 * width
+        # self.qimg = QImage(self.img, width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
 
-        # choose what label is gonna to show img.
-        if objectName == "label_img":
-            self.ui.label_img.setPixmap(QPixmap.fromImage(self.qimg))
-        elif objectName == "label_hist":
-            self.ui.label_hist.setPixmap(QPixmap.fromImage(self.qimg))
+        # # choose what label is gonna to show img.
+        # if objectName == "label_img":
+        #     self.ui.label_img.setPixmap(QPixmap.fromImage(self.qimg))
+        # elif objectName == "label_hist":
+        #     self.ui.label_hist.setPixmap(QPixmap.fromImage(self.qimg))
 
-
-    def open_and_return_filename(self):
-        self.img_name, _ = QFileDialog.getOpenFileName(self,
-                  "Open file",
-                  "./")   # start path
-        print(self.img_name)
-        self.img_name = self.img_name.split("/")[-1] # 取檔案路徑最後一個分割，即為檔名
-        self.ui.show_file_path1.setText(self.img_name) # show file path
-        self.display_img(img_name=self.img_name, objectName="label_img")
-        self.plot_histogram(self.img, objectName="label_hist")
 
     # plot histogram
-    def plot_histogram(self, img, objectName):
-        hist = np.zeros(256)
+    def plot_histogram(self, img, objectName="1"):
+        self.hist = np.zeros(256)
         # 計算各數值數量並儲存
         for i in np.unique(img):
-            hist[i] = np.bincount(img.flatten())[i]
-        # 利用長條圖繪出顯示
-        x_axis = np.arange(256)
+            self.hist[i] = np.bincount(img.flatten())[i]
+
+        self.x_axis = np.arange(256)
         plt.figure(figsize=(4,3))
-        plt.bar(x_axis, hist)
+        plt.bar(self.x_axis, self.hist)
         plt.title("Histogram")
 
         # show histogram
         plt.savefig("Matplotlib.jpg")
-        self.display_img(img_name="Matplotlib.jpg", objectName=objectName)
-        os.remove("Matplotlib.jpg")
+        # self.display_img(img_name="Matplotlib.jpg", objectName=objectName)
+        # os.remove("Matplotlib.jpg")
+
+
+    def get_qimg(self, img):
+        if type(img) == str: # 路徑的話就做imread,否則直接使用
+            self.img = cv2.imread(img)
+        elif type(img) == np.ndarray:
+             self.img = img
+        self.img = cv2.resize(self.img, (200,150))
+
+        # 記得對彩色圖及黑白圖片的QImage.Format、bytesPerline設定
+        # self.qimg = QImage(self.img, width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
+        if len(self.img.shape) == 3:
+            self.qimg = QImage(self.img, self.img.shape[1], self.img.shape[0], self.img.shape[1]*3, QImage.Format_RGB888).rgbSwapped()
+        elif len(self.img.shape) == 2:
+            self.qimg = QImage(self.img, self.img.shape[1], self.img.shape[0], self.img.shape[1], QImage.Format_Grayscale8).rgbSwapped()
+
+        return self.qimg
+
+
+
+    def Q2_1_open_file(self):
+        self.img_name, _ = QFileDialog.getOpenFileName(self,
+                "Open file",
+                "./")   # start path
+        # print(self.img_name)
+        self.img_name = self.img_name.split("/")[-1] # 取檔案路徑最後一個分割，即為檔名
+        self.ui.show_file_path1.setText(self.img_name) # show file path
+
+        self.qimg = self.get_qimg(self.img_name)
+        self.ui.label_img.setPixmap(QPixmap.fromImage(self.qimg))
+
+        self.plot_histogram(self.img)
+        self.qimg = self.get_qimg('Matplotlib.jpg')
+        self.ui.label_hist.setPixmap(QPixmap.fromImage(self.qimg))
+        os.remove('Matplotlib.jpg')
+
+
 
     def Q2_2_grayscale(self):
-        img = cv2.imread(self.img_name)
+        self.img = cv2.imread(self.img_name)
 
-        # split color channel
-        R = img[:,:,0]
-        G = img[:,:,1]
-        B = img[:,:,2]
-        gray_A = np.zeros_like(R) # 複製大小
-        gray_B = np.zeros_like(R)
+        # split color channel (opencv: BGR)
+        self.B = self.img[:,:,0]
+        self.G = self.img[:,:,1]
+        self.R = self.img[:,:,2]
+        self.gray_A = np.zeros_like(self.R) # 複製圖像大小
+        self.gray_B = np.zeros_like(self.G)
 
-        for i in range(img.shape[0]):
-            for n in range(img.shape[1]):
-                gray_A[i,n] = (R[i,n]) + int(G[i,n]) + int(B[i,n]) // 3
-                gray_B[i,n] = int(0.299 * int(R[i,n]) + 0.587 * int(G[i,n]) + 0.114 * int(B[i,n]))
+        for i in range(self.img.shape[0]):
+            for n in range(self.img.shape[1]):
+                self.gray_A[i,n] = (int(self.R[i,n]) + int(self.G[i,n]) + int(self.B[i,n])) / 3.0
+                self.gray_B[i,n] = int(0.299 * int(self.R[i,n]) + 0.587 * int(self.G[i,n]) + 0.114 * int(self.B[i,n]))
 
-        # TODO 參考https://codeantenna.com/a/sZw8f2E1Ie精簡化顯示照片
+        print(self.img.shape)
+        print(self.gray_A.shape)
+        print(self.gray_B.shape)
 
-        # self.display_img(gray_A, "label_img_2")
-        QImage(gray_A, gray_A[1], gray_A[0], gray_A[1] * 3, QImage.Format_RGB888).rgbSwapped()  # 此处如果不加*3，就会发生倾斜
-        self.plot_histogram(gray_A, 'label_hist_2')
-        self.display_img(gray_B, "label_img_3")
-        self.plot_histogram(gray_B, 'label_hist_3')
 
+        # TODO 參考https://codeantenna.com/a/sZw8f2E1Ie 精簡化顯示照片
+        self.qimg1 = self.get_qimg(self.gray_A)
+        self.ui.label_img_2.setPixmap(QPixmap.fromImage(self.qimg1))
+
+        print(type(self.qimg1))
+
+        self.qimg2 = self.get_qimg(self.gray_B)
+        self.ui.label_img_3.setPixmap(QPixmap.fromImage(self.qimg2))
+
+        # self.plot_histogram(self.gray_A, 'label_hist_2')
+        # self.display_img(self.gray_B, "label_img_3")
+        # self.plot_histogram(self.gray_B, 'label_hist_3')
+
+### TODO check shape 位置
 
 
     def Q2_3_histogram(self):
--- HW/HW02/ex02_WaitkeyDelay.py
+++ HW/HW02/ex02_WaitkeyDelay.py
@@ -3,21 +3,39 @@
 # ex02
 
 import cv2
+import numpy as np
 
 img = cv2.imread("lena.jpg")
 
+# split color channel
 R = img[:,:,0]
 G = img[:,:,1]
 B = img[:,:,2]
+gray_A = np.zeros_like(R) # 複製大小
+gray_B = np.zeros_like(R)
 
-print(img.shape)
-print(R.shape)
+for i in range(img.shape[0]):
+    for n in range(img.shape[1]):
+        gray_A[i,n] = (int(R[i,n]) + int(G[i,n]) + int(B[i,n])) // 3
+        gray_B[i,n] = int(0.3 * int(R[i,n]) + 0.1 * int(G[i,n]) + 0.1 * int(B[i,n]))
+
+print(len(img.shape))
+print(len(R.shape))
 
 
+# print(img.shape)
+# print(R.shape)
+# print(gray_B.shape[0])
 
-# cv2.imshow('R', R)
-# cv2.imshow('G', G)
-# cv2.imshow('B', B)
+# # img = cv2.resize(img, (200,150))
+# # img = cv2.resize(img, (200,150))
+# # img = cv2.resize(img, (200,150))
+
+# print(img.shape)
+
+# cv2.imshow('A', gray_A)
+# cv2.imshow('B', gray_B)
+# cv2.imshow('img', img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
@@ -26,3 +44,4 @@ print(R.shape)
 # print(type(img_name))
 # if type(img_name) == str: # 路徑的話就做imread,否則直接用
 #     print("yes")
+# %%
